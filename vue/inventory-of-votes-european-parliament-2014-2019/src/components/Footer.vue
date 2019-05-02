8492f00ab2ffd773e2f3d62aca3280963cafcb19
Michal Skop <michal.skop@kohovolit.eu>
2019-05-02 16:11:37 +0200

    inventory

---
+++ vue/inventory-of-votes-european-parliament-2014-2019/src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+    <footer class="text-muted mt-4 md-4">
+        <div class="container">
+            <div class="row justify-center">
+                <div class="col-md-3">
+
+                </div>
+                <div class="col-md-6 text-center">
+                    <a href="http://kohovolit.eu" title="KohoVolit.eu" id="footer-kohovolit-logo" target="_blank"><img src="https://volebnikalkulacka.cz/image/logo.png" alt="logo kohovolit.eu"></a>
+					<p class="lead black">Election Calculator is brought to you by <a href="http://kohovolit.eu" target="_blank">KohoVolit.eu</a> since 2006</p>
+                    <a href="https://europeum.org" target="_blank" alt="Europeum"><img src="@/assets/img/europeum.png" /></a>
+                    <p class="lead black">
+                        This calculator was made with cooperation with <a href="https://europeum.org" target="_blank">Europeum</a>.
+                    </p>
+                    <hr>
+					<p class="black">Creative Commons BY 4.0 | <a href="http://kohovolit.eu" id="footer-kohovolit-licence">KohoVolit.eu</a> 2019</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script>
+
+</script>
---
+++ vue/inventory-of-votes-european-parliament-2014-2019/vue.config.js
@@ -0,0 +1,4 @@
+// vue.config.js
+module.exports = {
+    publicPath: "/en/inventory-of-votes-european-parliament-2014-2019/"
+}
